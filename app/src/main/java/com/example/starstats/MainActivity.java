ccbfde1eeb3d9eeb75e5283dcd0cb699840bc332
josh <joecody@iu.edu>
2021-11-04 03:41:02 -0400

    can sucessfully create get requests to the api

--- app/src/main/java/com/example/starstats/MainActivity.java
+++ app/src/main/java/com/example/starstats/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,52 +12,72 @@ import android.widget.TextView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity implements Runnable {
 
+
+    public MainActivity() {}
+    public MainActivity(String tag) {this.tag = tag;}
+
     ConstraintLayout mainLayout;
+    Handler handler;
+    String tag;
     FloatingActionButton search;
-    TextView title;
+    TextView title, stats;
     EditText tagInput;
-    URLConnection connection;
+    HttpsURLConnection connection;
     String tok = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImIwNGRhYTFiLTgxMDMtNGE2ZC05NjgxLWYzZGFlMzFlMjA0NSIsImlhdCI6MTYzNTQ2NTkxNCwic3ViIjoiZGV2ZWxvcGVyL2EzZWM3MTk2LTM3NDMtYTIyOS02ZjM2LWVkN2U1NGM1MWZjOCIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTczLjE2NS4zMi4yMSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.Hw5ZIXewqQp-IGcC2iUoXap9KACNV656ei2aT0CFN93lews4EmgCEM0KSQhwdMm1jZbUAA5NsDJYCRfUJJij5A";
-    String url = "https://api.brawlstars.com/v1/players/%23";
+    String url = "https://api.brawlstars.com/v1/players/";
     Thread apiThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mainLayout = findViewById(R.id.enterID); search = findViewById(R.id.search); title = findViewById(R.id.starTitle); tagInput = findViewById(R.id.tagInput);
-
-
-        search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Implement API calls", Snackbar.LENGTH_LONG).show();
-                MainActivity m = new MainActivity();
-                apiThread = new Thread(m);
-                apiThread.start();
-            }
+        mainLayout = findViewById(R.id.enterID); search = findViewById(R.id.search); title = findViewById(R.id.starTitle); tagInput = findViewById(R.id.tagInput); stats = findViewById(R.id.stats);
+        handler = new Handler();
+        search.setOnClickListener(view -> {
+            tag = getTag();
+            MainActivity m = new MainActivity(tag);
+            apiThread = new Thread(m);
+            apiThread.start();
         });
     }
+    public String getTag() {  return tagInput.getText().toString();  }
 
     @Override
     public void run() {
         System.out.println("API Thread has started running");
-                String tag = tagInput.getText().toString();
-                try {
-                    connection = new URL(url+tag).openConnection();
-                    connection.setRequestProperty("authorization", tok);
-                    //InputStream is = connection.getInputStream();
-                    System.out.println(connection.getContent());
-                } catch (IOException e) {
-                    System.out.println("Connection did not work");
-                    e.printStackTrace();
-                }
+        try {
+                connection = (HttpsURLConnection) new URL(url+ "%23" + this.tag).openConnection();
+                connection.setRequestMethod("GET");
+                connection.addRequestProperty("authorization", tok);
+                connection.addRequestProperty("Accept", "application/json");
+                connection.setDoInput(true);
+                connection.setDoOutput(false);
+                InputStream is = connection.getInputStream();
+                System.out.println(inputStreamToString(is));
+        } catch (IOException e) { e.printStackTrace(); }
+    }
+
+    private String inputStreamToString(InputStream is) throws IOException {
+        InputStreamReader isReader = new InputStreamReader(is);
+        BufferedReader reader = new BufferedReader(isReader);
+        StringBuilder sb = new StringBuilder();
+        String str;
+        while((str = reader.readLine())!=null)
+            sb.append(str);
+        return sb.toString();
     }
 }
